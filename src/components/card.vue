a74ee9e8c78963ca14ecc7955ac58a84620e0127
yzq <739291773@qq.com>
2018-10-17 10:34:06 +0800

    图书列表的渲染

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,93 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
+  <div class="book-card">
+    <div class="thumb" @click.stop="preview">
+      <img :src="book.image" 
+            class="img"
+            mode="aspectFit"
+            />
+    </div>
+    <div class="detail">
+      <div class="row text-primary">
+        <div class="right">
+          {{book.rate}} <Rate :value='book.rate'></Rate>
+        </div>
+        <div class="left">
+          {{book.title}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="right text-primary">
+          浏览量:{{book.count}}
+        </div>
+        <div class="left">
+          
+          {{book.author}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="right">
+          添加人
+           <!-- {{book.user_info.nickName}} -->
+        </div>
+        <div class="left">
+         {{book.publisher}}
+        </div>
+      </div>
+
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['text']
+  props: ['book'],
+  components:{
+
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
 }
 </script>
 
-<style>
-.card {
-  padding: 10px;
+<style lang='scss' scoped>
+.book-card{
+  padding:5px;
+  overflow: hidden;
+  margin-top:5px;
+  margin-bottom:5px;
+  font-size:14px;
+  .thumb{
+    width:90px;
+    height:90px;
+    float: left;
+    margin:0 auto;
+    overflow:hidden;
+    .img{
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
+  .detail{
+    margin-left: 100px;
+    .row{
+      line-height:20px;
+      margin-bottom:3px;
+    }
+    .right{
+      float: right;
+      
+    }
+    .left{
+      margin-right:80px;
+    }
+  }
+  
 }
 </style>
--- src/pages/books/index.vue
+++ src/pages/books/index.vue
@@ -1,24 +1,29 @@
 <template>
     <div>
-        图书列表
+        <Card :key="book.id" v-for="book in books" :book="book"></Card>
     </div>
 </template>
 
 <script>
 import {get} from '../../utils/index.js'
+import Card from '@/components/card.vue'
 export default {
+    components:{
+        Card
+    },
     data(){
         return {
             books:[]
         }
     },
-    mounted() {
+    mounted(){
+        console.log('我被挂载了')
         this.getList()
     },
     methods:{
         async getList(){
             const books = await get('/weapp/booklist',{})
-            console.log('图书列表',books)
+            this.books= books.list
         }
     }
 
