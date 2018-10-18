352a86a712e161d74e98812c034ae78b53db4f39
yzq <739291773@qq.com>
2018-10-18 09:41:21 +0800

    修改星级组件偏小的问题

--- src/components/Rate.vue
+++ src/components/Rate.vue
@@ -18,20 +18,23 @@ export default {
   },
   computed:{
     style(){
-      return `width:${this.value/2}em`
+      return `width:${(this.value*0.8)/2}em`
     }
   }
 }
 </script>
 <style lang='scss'>
 .rate{
-  font-size: 1em;
+  font-size: 16px;
+  width: 5em;
+  // border: 1px solid black;
   position: relative;
   display: inline-block;
   .hollow{
-    font-size: 1em;
+    font-size: 16px;
     position: absolute;
     display: inline-block;
+    // top:100%;
     top:0;
     left:0;
     width:0;
